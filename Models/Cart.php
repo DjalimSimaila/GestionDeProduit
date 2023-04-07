f7e4cc5d61373570aa7f407095788782d9b17786
Djalim Simaila <djalim.simaila@etu.univ-amu.fr>
2023-04-07 18:34:45 +0200

    commit atomique 2

--- Models/Cart.php
+++ Models/Cart.php
@@ -11,45 +11,13 @@ class Cart {
     }
 
     public static function createFull(string $S_name, int $I_user_id): Cart {
-        return new Cart();
-    }
-    public function update(){
-        $curlConnection  = curl_init();
-        $params = array(
-            CURLOPT_URL =>  "http://localhost:8080/cart/".$this->I_id,
-            CURLOPT_PUT => 1,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => "name=".$this->S_name."&user_id=".$this->I_user_id,
-        );
-        curl_setopt_array($curlConnection, $params);
-        $response = curl_exec($curlConnection);
-        curl_close($curlConnection);
-    }
-    public function insert(){
-        $curlConnection  = curl_init();
-        $params = array(
-            CURLOPT_URL =>  "http://localhost:8080/cart",
-            CURLOPT_POST => 1,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => "name=".$this->S_name."&user_id=".$this->I_user_id,
-        );
-        curl_setopt_array($curlConnection, $params);
-        $response = curl_exec($curlConnection);
-        curl_close($curlConnection);
+        $O_cart = new Cart();
+        $O_cart->S_name = $S_name;
+        $O_cart->I_user_id = $I_user_id;
+        return $O_cart;
 
-        foreach ($this->M_products as $product => $quantity){
-            $curlConnection  = curl_init();
-            $params = array(
-                CURLOPT_URL =>  "http://localhost:8080/cart/".$response."/product/".$product->I_id,
-                CURLOPT_POST => 1,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_POSTFIELDS => "quantity=".$quantity,
-            );
-            curl_setopt_array($curlConnection, $params);
-            $response = curl_exec($curlConnection);
-            curl_close($curlConnection);
-        }
     }
+
     public static function getById(int $I_id): Cart {
         $curlConnection  = curl_init();
         $params = array(
@@ -69,7 +37,102 @@ class Cart {
         }
         return $O_cart;
     }
+
+    public function insert(){
+        $O_data = array(
+            "name" => $this->S_name,
+            "user_id" => $this->I_user_id,
+        );
+        $curlConnection  = curl_init();
+        $params = array(
+            CURLOPT_URL =>  "http://localhost:8080/cart",
+            CURLOPT_POST => 1,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => json_encode($O_data)
+        );
+        curl_setopt_array($curlConnection, $params);
+        $response = curl_exec($curlConnection);
+        curl_close($curlConnection);
+
+        foreach ($this->M_products as $product => $quantity){
+            $curlConnection  = curl_init();
+            $params = array(
+                CURLOPT_URL =>  "http://localhost:8080/cart/".$response."/product/".$product->I_id,
+                CURLOPT_POST => 1,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_POSTFIELDS => "quantity=".$quantity,
+            );
+            curl_setopt_array($curlConnection, $params);
+            $response = curl_exec($curlConnection);
+            curl_close($curlConnection);
+        }
+    }
+
+    public function update(){
+        $A_products = array();
+        
+        foreach($this->M_products as $product => $quantity){
+            $A_products[] = array(
+                "product_id" => $product->I_id,
+                "quantity" => $quantity
+            );
+        };
+
+        $O_data = array(
+            "name" => $this->S_name,
+            "user_id" => $this->I_user_id,
+            "products" => $A_products
+        );
+
+        $curlConnection  = curl_init();
+        $params = array(
+            CURLOPT_URL =>  "http://localhost:8080/cart/".$this->I_id,
+            CURLOPT_PUT => 1,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => json_encode($O_data)
+        );
+        curl_setopt_array($curlConnection, $params);
+        $response = curl_exec($curlConnection);
+        curl_close($curlConnection);
+    }
+
     public function addProduct(Product $O_product,int $I_quantity) {
         $this->M_products[$O_product] = $I_quantity;
+        $O_data = array(
+            "product_id" => $O_product->I_id,
+            "quantity" => $I_quantity
+        );
+        $curlConnection  = curl_init();
+        $params = array(
+            CURLOPT_URL =>  "http://localhost:8080/cart/".$this->I_id."/product/".$O_product->I_id,
+            CURLOPT_POST => 1,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => json_encode($O_data),
+        );
+    }
+
+    public function deleteProduct(Product $O_product) {
+        unset($this->M_products[$O_product]);
+        $curlConnection  = curl_init();
+        $params = array(
+            CURLOPT_URL =>  "http://localhost:8080/cart/".$this->I_id."/product/".$O_product->I_id,
+            CURLOPT_CUSTOMREQUEST => "DELETE",
+            CURLOPT_RETURNTRANSFER => true,
+        );
+        curl_setopt_array($curlConnection, $params);
+        $response = curl_exec($curlConnection);
+        curl_close($curlConnection);
+    }
+
+    public function delete() {
+        $curlConnection  = curl_init();
+        $params = array(
+            CURLOPT_URL =>  "http://localhost:8080/cart/".$this->I_id,
+            CURLOPT_CUSTOMREQUEST => "DELETE",
+            CURLOPT_RETURNTRANSFER => true,
+        );
+        curl_setopt_array($curlConnection, $params);
+        $response = curl_exec($curlConnection);
+        curl_close($curlConnection);
     }
 }
--- Models/Product.php
+++ Models/Product.php
@@ -6,8 +6,8 @@ class Product {
     public int $I_quantity = 0;
     public float $F_price = 0;
 
-
-    public static function createEmpty(){
+    public static function createEmpty()
+    {
         return new Product();
     }
 
@@ -22,21 +22,26 @@ class Product {
 
     public function insert()
     {
+        $O_data = array(
+            "name" => $this->S_name,
+            "price" => $this->F_price,
+            "quantity" => $this->I_quantity,
+        );
         $curlConnection  = curl_init();
         $params = array(
             CURLOPT_URL =>  "http://localhost:8080/product",
             CURLOPT_POST => 1,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => "name=".$this->S_name."&price=".$this->F_price."&quantity=".$this->I_quantity,
+            CURLOPT_POSTFIELDS => json_encode($O_data)
         );
         curl_setopt_array($curlConnection, $params);
         $response = curl_exec($curlConnection);
+        $this->I_id = json_decode($response)->id;
         curl_close($curlConnection);
     }
 
     public static function getById(int $I_id): Product
     {
-        //TODO Fix this
         return new Product();
         $curlConnection  = curl_init();
         $params = array(
@@ -57,12 +62,17 @@ class Product {
 
     public function update()
     {
+        $O_data = array(
+            "name" => $this->S_name,
+            "price" => $this->F_price,
+            "quantity" => $this->I_quantity,
+        );
         $curlConnection  = curl_init();
         $params = array(
             CURLOPT_URL =>  "http://localhost:8080/product/".$this->I_id,
             CURLOPT_PUT => 1,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => "name=".$this->S_name."&price=".$this->F_price."&quantity=".$this->I_quantity,
+            CURLOPT_POSTFIELDS => json_encode($O_data),
         );
         curl_setopt_array($curlConnection, $params);
         $response = curl_exec($curlConnection);
